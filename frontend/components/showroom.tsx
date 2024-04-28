42ed535e5adad314644caa99dc14c6337697628a
Viktor Shen <viktorshenofficial@gmail.com>
2024-04-28 03:37:11 +0200

    changes

--- frontend/components/showroom.tsx
+++ frontend/components/showroom.tsx
@@ -1,4 +1,18 @@
+import { Button } from "@nextui-org/react";
 import { LayoutGroup, motion } from "framer-motion";
+import Modal from "react-modal";
+import { DayPicker } from "react-day-picker";
+import {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useContext,
+  ReactNode,
+} from "react";
+import { toast } from "sonner";
+import "react-day-picker/dist/style.css";
+import { FaXmark } from "react-icons/fa6";
 
 interface ShowroomProps {
   showroom: any;
@@ -7,6 +21,65 @@ interface ShowroomProps {
 function numberWithCommas(x: number) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+interface ToolProps {
+  tool_call: any;
+}
+
+const ScheduleMeetingTool = ({ tool_call }: ToolProps) => {
+  // const { selectedMeeting, meetings, chatEnded, sendAction } =
+  //   useContext(FUContext);
+  // const disabled =
+  //   selectedMeeting != meetings[meetings.length - 1].timestamp || chatEnded;
+
+  // let parsed = undefined;
+  // try {
+  //   parsed = JSON.parse(tool_call.function.arguments);
+  //   const { year, month, day, hour, minute } = parsed;
+  //   parsed = new Date(year, month - 1, day, hour, minute);
+  // } catch (e) {
+  //   parsed = undefined;
+  // }
+
+  const [selected, setSelected] = useState<Date | undefined>(undefined);
+
+  const current = selected;
+  return (
+    <div>
+      {
+        <DayPicker
+          // key={selectedMeeting}
+          className="bg-white "
+          style={{ margin: "0px" }}
+          mode="single"
+          selected={current}
+          onSelect={setSelected}
+          // disabled={disabled}
+          footer={
+            <div className="flex flex-col justify-center">
+              {/* <p className="text-primary/100 font-bold text-center my-3">
+                Your meeting will be on {current!.toLocaleDateString()}.
+              </p> */}
+              <Button
+                className="bg-black rounded-sm text-white mt-4"
+                // isDisabled={disabled}
+                onClick={() => {
+                  // sendAction({
+                  //   type: "SCHEDULE_MEETING",
+                  //   timestamp: current.getTime() / 1000,
+                  // });
+                  toast.info("Scheduling meeting...");
+                }}
+              >
+                Schedule Test Drive
+              </Button>
+            </div>
+          }
+        />
+      }
+    </div>
+  );
+};
 
 const Showroom = ({ showroom }: ShowroomProps) => {
   //   const carList: any[] = [1, 2, 3, 4, 5, 6, 6, 3, 3, 3, 3, 3];
@@ -45,63 +118,111 @@ const Showroom = ({ showroom }: ShowroomProps) => {
   //   },
   // ];
   const carList = showroom.showing;
+  console.log(carList);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
 
   return (
-    <LayoutGroup>
-      <div className="pl-10 text-3xl font-serif p-6 bg-black text-white text-center flex align-middle">
-        <img src="mercedes_logo.svg" alt="" className="h-10" />
-        <h1 className="ml-5 text-xl text-slate-200 self-center">
-          Mercedes Showroom
-        </h1>
-      </div>
-      {/* <p>Here are some amazing options I compiled for you.</p> */}
-      <motion.div
-        // style={{
-        //   backgroundImage: "url(background_merc.png)",
-        //   backgroundSize: "100% auto",
-        //   backgroundRepeat: "no-repeat",
-        // }}
-        className="flex flex-wrap  items-start gap-5 p-10 pt-8 w-full h-full overflow-scroll scrollbar-hide"
-      >
-        {/* <div className="bg-white w-full rounded-md shadow-[0_0px_2px_0.5px_rgba(0,0,0,0.32)] p-5">
+    <div className="h-full">
+      {/* <button onClick={openModal}>Open Schedule Meeting Tool</button> */}
+      <LayoutGroup>
+        <div className="pl-10 text-3xl font-serif p-6 bg-black text-white text-center flex align-middle">
+          <img src="mercedes_logo.svg" alt="" className="h-10" />
+          <h1 className="ml-5 text-xl text-slate-200 self-center">
+            Mercedes Showroom
+          </h1>
+        </div>
+        {carList.length > 0 ? (
+          <motion.div
+            // style={{
+            //   backgroundImage: "url(background_merc.png)",
+            //   backgroundSize: "100% auto",
+            //   backgroundRepeat: "no-repeat",
+            // }}
+            className="flex flex-wrap  items-start gap-5 p-10 pt-8 w-full h-full overflow-scroll scrollbar-hide"
+          >
+            {/* <div className="bg-white w-full rounded-md shadow-[0_0px_2px_0.5px_rgba(0,0,0,0.32)] p-5">
           Here are some amazing options I compiled for you!
         </div> */}
-        <p className="font-serif text-3xl">Here are our Family SUVs</p>
-        <div className="bg-white w-full flex items-center">
-          {/* <div className="rounded-md shadow-[0_0px_2px_0.5px_rgba(0,0,0,0.32)] p-2 px-3 mr-4 font-light ">
+            <p className="font-serif text-3xl">Here are our Family SUVs</p>
+            <div className="bg-white w-full flex items-center">
+              {/* <div className="rounded-md shadow-[0_0px_2px_0.5px_rgba(0,0,0,0.32)] p-2 px-3 mr-4 font-light ">
             Status: Available
           </div> */}
 
-          <div className="font-light pb-2">Showing the best 3 options</div>
-        </div>
-        {carList.map((car: any) => (
-          <motion.div
-            layout
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            // className={`flex flex-row bg-default-50 rounded-2xl focus:ring-4 ring-primary/100 w-96 justify-center`}
-            className={`bg-white rounded-md shadow-[0_0px_2px_0.5px_rgba(0,0,0,0.32)] w-72 h-[550px] p-10 transition-transform duration-500 hover:cursor-pointer`} //hover:shadow-[0_2px_15px_1px_rgba(0,0,0,0.24)]
-          >
-            <img
-              className="transition-transform duration-300 hover:scale-110 h-24 m-auto"
-              src={car.image_url[0]}
-              alt=""
-            />
-            <p className="font-bold mt-4 text-lg">{car.name}</p>
-            <p className="font-light text-sm">
-              {car.vehicle_type} • {car.num_seats} Seats • {car.powertrain_type}
-            </p>
-            <p className="font-bold text-sm mt-1">
-              €{numberWithCommas(car.price_in_usd)}
-            </p>
-            {/* <button className="bg-[#0078d6] text-white mt-4 p-2 w-full">
+              <div className="font-light pb-2">Showing the best 3 options</div>
+            </div>
+            {carList.map((car: any) => (
+              <motion.div
+                layout
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                // className={`flex flex-row bg-default-50 rounded-2xl focus:ring-4 ring-primary/100 w-96 justify-center`}
+                className={`bg-white rounded-md shadow-[0_0px_2px_0.5px_rgba(0,0,0,0.32)] w-72 h-[550px] p-10 transition-transform duration-500 hover:cursor-pointer`} //hover:shadow-[0_2px_15px_1px_rgba(0,0,0,0.24)]
+              >
+                <img
+                  className="transition-transform duration-300 hover:scale-110 h-24 m-auto"
+                  src={car.image_url[0]}
+                  alt=""
+                />
+                <p className="font-bold mt-4 text-lg">{car.specs.model}</p>
+                <p className="font-light text-sm">
+                  {car.vehicle_type} • {car.num_seats} Seats •{" "}
+                  {car.powertrain_type}
+                </p>
+                <p className="font-bold text-sm mt-1">
+                  €{numberWithCommas(car.price_in_usd)}
+                </p>
+                {/* <button className="bg-[#0078d6] text-white mt-4 p-2 w-full">
               I like this
             </button> */}
+              </motion.div>
+            ))}
+            <div className="p-44"></div>
           </motion.div>
-        ))}
-        <div className="p-44"></div>
-      </motion.div>
-    </LayoutGroup>
+        ) : (
+          <div className="flex w-full h-full pb-36 items-center justify-center">
+            <p className="font-serif text-center text-4xl">
+              Welcome to the Showroom! 💫
+            </p>
+          </div>
+        )}
+      </LayoutGroup>
+      {modalIsOpen && (
+        <div className="flex justify-around bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-xl rounded-md  p-10  border-black border-2">
+          {/* w-[700px] */}
+          {/* <div className="">
+            <img
+              className="transition-transform duration-300 hover:scale-110 h-24 m-auto"
+              src={carList[0].image_url[0]}
+              alt=""
+            />
+            <p className="font-bold mt-4 text-lg">{carList[0].specs.model}</p>
+            <p className="font-light text-sm">
+              {carList[0].vehicle_type} • {carList[0].num_seats} Seats •{" "}
+              {carList[0].powertrain_type}
+            </p>
+            <p className="font-bold text-sm mt-1">
+              €{numberWithCommas(carList[0].price_in_usd)}
+            </p>
+          </div> */}
+          <div className="flex flex-col items-end">
+            <button className="mb-8" onClick={closeModal}>
+              <FaXmark></FaXmark>
+            </button>
+            <ScheduleMeetingTool tool_call={undefined} />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
